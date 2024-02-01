86f4cb7f7c7d14c8560841772720e09ca0ede37c
JAIKISHEN-MISHRA <119570134+JAIKISHEN-MISHRA@users.noreply.github.com>
2024-02-01 18:54:45 +0530

    profile bug

--- Backend/routes/friendrequest.js
+++ Backend/routes/friendrequest.js
@@ -64,6 +64,24 @@ friendRouter.route('/friend-requests').get(protect, async (req, res) => {
   }
 });
 
+friendRouter.route('/friend-requests/:userId').get(protect, async (req, res) => {
+  try {
+    const receiverId = req.params.userId;
+    const senderId=req.user._id
+    const friendRequests = await FriendRequest.find({
+      sender:senderId,
+      receiver: receiverId,
+      status: 'pending',
+    })
+      .populate('sender', 'username'); // Populate sender information
+
+    res.json(friendRequests);
+  } catch (error) {
+    console.error('Error fetching friend requests:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
 
   friendRouter.post('/:id/accept', async (req, res) => {
     try {
