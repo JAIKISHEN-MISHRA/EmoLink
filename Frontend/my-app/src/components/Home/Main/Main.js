e043aa60e1ebc497bf0a46b016233b7839de1cae
JAIKISHEN-MISHRA <119570134+JAIKISHEN-MISHRA@users.noreply.github.com>
2024-02-26 10:51:36 +0530

    Minor change

--- Frontend/my-app/src/components/Home/Main/Main.js
+++ Frontend/my-app/src/components/Home/Main/Main.js
@@ -15,6 +15,7 @@ import { BsPlusCircle } from 'react-icons/bs';
 import { BsImages } from "react-icons/bs";
 import Sidebar from "../Sidebar/Sidebar.js";
 import Navbar from "../Navbar/Navbar.js";
+import { Theme } from "./Theme/Theme.js";
 const Main = () => {
     const [u , setU] =useState([])
     const [loading, setLoading] = useState(false);
@@ -39,6 +40,7 @@ const Main = () => {
                 const email = localStorage.getItem('token');
                 const response = await axios.get(`http://localhost:5000/profDetail?email=${email}`);
                 const user = response.data.user;
+                console.log(user);
                 setU(user);
             } catch (error) {
                 console.log(error);
@@ -116,7 +118,7 @@ const Main = () => {
 
 
         fetchStories();
-
+        fetchProfName();
 
         fetchFriendRequestsData();
         fetchUsers();
@@ -321,8 +323,8 @@ const Main = () => {
                                                 </div>
                                                 <div className="heading" id="heading2">
                                                     <div className="profile-photo">
-                                                                    {u.profile ? (
-                                                                        <img src={u.profile} alt="Profile" />
+                                                                    {u.profilePicture ? (
+                                                                        <img src={u.profilePicture} alt="Profile" />
                                                                     ) : (
                                                                         <img src={Logo} alt="Profile" />
                                                                     )}
@@ -429,56 +431,7 @@ const Main = () => {
                     )}
                 </>)}
 
-            <div className="customize-theme">
-                <div className="card">
-                    <h2>Customize your theme</h2>
-                    <p>Manage your font size, color, and background</p>
-
-                    <div className="font-size">
-                        <h4>Font Size</h4>
-                        <div>
-                            <h5>Aa</h5>
-                            <div className="choose-size">
-                                <span className="font-size-1 active"></span>
-                                <span className="font-size-2"></span>
-                                <span className="font-size-3"></span>
-                                <span className="font-size-4"></span>
-                                <span className="font-size-5"></span>
-                            </div>
-                            <h3>Aa</h3>
-                        </div>
-                    </div>
-
-                    <div className="color">
-                        <h4>Color</h4>
-                        <div className="choose-color">
-                            <span className="color-1 active"></span>
-                            <span className="color-2"></span>
-                            <span className="color-3"></span>
-                            <span className="color-4"></span>
-                            <span className="color-5"></span>
-                        </div>
-                    </div>
-
-                    <div className="background">
-                        <h4>Background</h4>
-                        <div className="choose-bg">
-                            <div className="bg-1 active">
-                                <span></span>
-                                <h5 htmlFor="bg-1">Light</h5>
-                            </div>
-                            <div className="bg-2">
-                                <span></span>
-                                <h5 htmlFor="bg-2">Dim</h5>
-                            </div>
-                            <div className="bg-3">
-                                <span></span>
-                                <h5 htmlFor="bg-3">Lights out</h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Theme/>
         </>
     );
 };
